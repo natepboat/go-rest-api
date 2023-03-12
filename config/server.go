36d5357c7e0f3334cb8fcefca68ae04cde481d5a
natepboat <natep.boat000@gmail.com>
2023-03-12 19:36:59 +0700

    refactor server config

--- config/server.go
+++ config/server.go
@@ -14,19 +14,26 @@ import (
 
 func ConfigServer(app goappenv.IAppEnv, provider *provider.ComponentProvider) *http.Server {
 	r := gorouter.NewRouter(app, nil)
-	authInterceptor := provider.Required("interceptor.AuthInterceptor").(interceptor.IHttpInterceptor)
-	metricInterceptor := provider.Required("interceptor.MetricInterceptor").(interceptor.IHttpInterceptor)
-
-	s := provider.Required("api.StatController").(*api.StatController)
-	u := provider.Required("api.UserController").(*api.UserController)
-
-	r.AddRoute(httpMethod.GET, "/", interceptor.Intercept(s.Home, metricInterceptor))
-	r.AddRoute(httpMethod.GET, "/user/:id", u.GetUser)
-	r.AddRoute(httpMethod.GET, "/v2/user/:id", interceptor.Intercept(u.GetUser, authInterceptor, metricInterceptor))
+	routeMapping(r, provider)
 
 	server, err := r.NewServer()
 	if err != nil {
 		log.Fatalln("Cannot create route server", err)
 	}
 	return server
+}
+
+func routeMapping(r *gorouter.Router, provider *provider.ComponentProvider) {
+	// interceptor
+	authInterceptor := provider.Required("interceptor.AuthInterceptor").(interceptor.IHttpInterceptor)
+	metricInterceptor := provider.Required("interceptor.MetricInterceptor").(interceptor.IHttpInterceptor)
+
+	// controller
+	statController := provider.Required("api.StatController").(*api.StatController)
+	userController := provider.Required("api.UserController").(*api.UserController)
+
+	// route mapping
+	r.AddRoute(httpMethod.GET, "/", interceptor.Intercept(statController.Home, metricInterceptor))
+	r.AddRoute(httpMethod.GET, "/user/:id", userController.GetUser)
+	r.AddRoute(httpMethod.GET, "/v2/user/:id", interceptor.Intercept(userController.GetUser, authInterceptor, metricInterceptor))
 }
